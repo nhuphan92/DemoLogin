a036a07763bcfa0fd2b0f848151703bebce2b230
Nhu Phan <nhu.phan@eastagile.com>
2017-12-14 09:47:12 +0700

    Add test for TestValidator

--- DemoLogin/DemoLoginTests/TextValidatorTest.m
+++ DemoLogin/DemoLoginTests/TextValidatorTest.m
@@ -7,6 +7,11 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <ReactiveObjC/ReactiveObjC.h>
+#import <OCMock/OCMock.h>
+#import "TextValidator.h"
+#import "TextValidationRule.h"
+
 
 @interface TextValidatorTest : XCTestCase
 
@@ -24,16 +29,122 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testValidTextValidateFailly {
+    id textValidator = OCMPartialMock([TextValidator new]);
+    NSString *testText = @"Password";
+    NSString *messageError = @"error";
+    TextValidationRuleRequired *requireRule = OCMPartialMock([[TextValidationRuleRequired alloc] initWithMessage:messageError]);
+    OCMStub([requireRule isValidWithText:testText]).andReturn(NO);
+    ValidationResult* result = [textValidator isValidWithText:testText andRules:@[requireRule]];
+    XCTAssert(result != nil);
+    XCTAssert(result.result == ValidationResultTypeFail);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testValidTextValidateSuccessfully {
+    id textValidator = OCMPartialMock([TextValidator new]);
+    NSString *testText = @"Password";
+    NSString *messageError = @"error";
+    TextValidationRuleRequired *requireRule = OCMPartialMock([[TextValidationRuleRequired alloc] initWithMessage:messageError]);
+    OCMStub([requireRule isValidWithText:testText]).andReturn(YES);
+    ValidationResult* result = [textValidator isValidWithText:testText andRules:@[requireRule]];
+    XCTAssert(result != nil);
+    XCTAssert(result.result == ValidationResultTypeOk);
+}
+
+- (void)testValidTextSuccessful {
+    id textValidator = OCMPartialMock([TextValidator new]);
+    NSString *testText = @"Password";
+    ValidationResult* result = [textValidator isValidWithText:testText andRules:@[]];
+    XCTAssert(result != nil);
+    XCTAssert(result.result == ValidationResultTypeOk);
+}
+
+- (void)testValidTextMultipleRulesExpectedFailAtFirstRule {
+    id textValidator = OCMPartialMock([TextValidator new]);
+    NSString *testText = @"";
+    NSString *messageErrorRequired = @"error required";
+    NSString *messageErrorMinimum = @"error minimum size";
+    NSString *messageErrorMaximum = @"error maximum size";
+    
+    TextValidationRuleRequired *requireRule = OCMPartialMock([[TextValidationRuleRequired alloc] initWithMessage:messageErrorRequired]);
+    OCMStub([requireRule isValidWithText:testText]).andReturn(NO);
+    
+    TextValidationRuleMinimum *minimumRule = OCMPartialMock([[TextValidationRuleMinimum alloc] initWithMessage:messageErrorMinimum minimumSize:5]);
+    OCMStub([minimumRule isValidWithText:testText]).andReturn(NO);
+    
+    TextValidationRuleMinimum *maximumRule = OCMPartialMock([[TextValidationRuleMaximum alloc] initWithMessage:messageErrorMaximum maximumSize:10]);
+    OCMStub([maximumRule isValidWithText:testText]).andReturn(NO);
+    
+    ValidationResult* result = [textValidator isValidWithText:testText andRules:@[requireRule, minimumRule, maximumRule]];
+    XCTAssert(result != nil);
+    XCTAssert(result.result == ValidationResultTypeFail);
+    XCTAssert([result.errorMessage isEqualToString:messageErrorRequired]);
+}
+
+- (void)testValidTextMultipleRulesExpectedFailAtSecondRule {
+    id textValidator = OCMPartialMock([TextValidator new]);
+    NSString *testText = @"abc";
+    NSString *messageErrorRequired = @"error required";
+    NSString *messageErrorMinimum = @"error minimum size";
+    NSString *messageErrorMaximum = @"error maximum size";
+    
+    TextValidationRuleRequired *requireRule = OCMPartialMock([[TextValidationRuleRequired alloc] initWithMessage:messageErrorRequired]);
+    OCMStub([requireRule isValidWithText:testText]).andReturn(YES);
+    
+    TextValidationRuleMinimum *minimumRule = OCMPartialMock([[TextValidationRuleMinimum alloc] initWithMessage:messageErrorMinimum minimumSize:5]);
+    OCMStub([minimumRule isValidWithText:testText]).andReturn(NO);
+    
+    TextValidationRuleMinimum *maximumRule = OCMPartialMock([[TextValidationRuleMaximum alloc] initWithMessage:messageErrorMaximum maximumSize:10]);
+    OCMStub([maximumRule isValidWithText:testText]).andReturn(NO);
+    
+    ValidationResult* result = [textValidator isValidWithText:testText andRules:@[requireRule, minimumRule, maximumRule]];
+    XCTAssert(result != nil);
+    XCTAssert(result.result == ValidationResultTypeFail);
+    XCTAssert([result.errorMessage isEqualToString:messageErrorMinimum]);
+}
+
+- (void)testValidTextMultipleRulesExpectedFailAtThirdRule {
+    id textValidator = OCMPartialMock([TextValidator new]);
+    NSString *testText = @"abcdefjghtedffsdd";
+    NSString *messageErrorRequired = @"error required";
+    NSString *messageErrorMinimum = @"error minimum size";
+    NSString *messageErrorMaximum = @"error maximum size";
+    
+    TextValidationRuleRequired *requireRule = OCMPartialMock([[TextValidationRuleRequired alloc] initWithMessage:messageErrorRequired]);
+    OCMStub([requireRule isValidWithText:testText]).andReturn(YES);
+    
+    TextValidationRuleMinimum *minimumRule = OCMPartialMock([[TextValidationRuleMinimum alloc] initWithMessage:messageErrorMinimum minimumSize:5]);
+    OCMStub([minimumRule isValidWithText:testText]).andReturn(YES);
+    
+    TextValidationRuleMinimum *maximumRule = OCMPartialMock([[TextValidationRuleMaximum alloc] initWithMessage:messageErrorMaximum maximumSize:10]);
+    OCMStub([maximumRule isValidWithText:testText]).andReturn(NO);
+    
+    ValidationResult* result = [textValidator isValidWithText:testText andRules:@[requireRule, minimumRule, maximumRule]];
+    XCTAssert(result != nil);
+    XCTAssert(result.result == ValidationResultTypeFail);
+    XCTAssert([result.errorMessage isEqualToString:messageErrorMaximum]);
+}
+
+- (void)testValidTextMultipleRulesExpectedPassAllRules {
+    id textValidator = OCMPartialMock([TextValidator new]);
+    NSString *testText = @"abcdefj";
+    NSString *messageErrorRequired = @"error required";
+    NSString *messageErrorMinimum = @"error minimum size";
+    NSString *messageErrorMaximum = @"error maximum size";
+    
+    TextValidationRuleRequired *requireRule = OCMPartialMock([[TextValidationRuleRequired alloc] initWithMessage:messageErrorRequired]);
+    OCMStub([requireRule isValidWithText:testText]).andReturn(YES);
+    
+    TextValidationRuleMinimum *minimumRule = OCMPartialMock([[TextValidationRuleMinimum alloc] initWithMessage:messageErrorMinimum minimumSize:5]);
+    OCMStub([minimumRule isValidWithText:testText]).andReturn(YES);
+    
+    TextValidationRuleMinimum *maximumRule = OCMPartialMock([[TextValidationRuleMaximum alloc] initWithMessage:messageErrorMaximum maximumSize:10]);
+    OCMStub([maximumRule isValidWithText:testText]).andReturn(YES);
+    
+    ValidationResult* result = [textValidator isValidWithText:testText andRules:@[requireRule, minimumRule, maximumRule]];
+    XCTAssert(result != nil);
+    XCTAssert(result.result == ValidationResultTypeOk);
+    XCTAssert([result.errorMessage isEqualToString:@""]);
 }
 
 @end
